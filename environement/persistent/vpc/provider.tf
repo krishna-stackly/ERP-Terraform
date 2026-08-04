abe0cba3cab7ec2ce099b3daf81db5c9dec80707
kriiishamtic <kriiishmatic@gmail.com>
2026-08-04 13:01:42 +0530

    all

--- environement/persistent/vpc/provider.tf
+++ environement/persistent/vpc/provider.tf
@@ -9,8 +9,8 @@ terraform {
   }
 
   backend "s3" {
-    bucket       = "YOUR-TERRAFORM-STATE-BUCKET"
-    key          = "devops-platform/network/terraform.tfstate"
+    bucket       = "terraform-backend-state-ecs"
+    key          = "devops-platform/network/vpc/terraform.tfstate"
     region       = "us-east-1"
     encrypt      = true
     use_lockfile = true
