a3403dee3dbd5fbe2765a1a2d2e2345c4e9de594
kriiishamtic <kriiishmatic@gmail.com>
2026-08-18 15:15:38 +0530

    all

---
+++ environement/persistent/shared-db/data.tf
@@ -0,0 +1,41 @@
+############################################
+# VPC ID from SSM
+############################################
+
+data "aws_ssm_parameter" "vpc_id" {
+  name = "/${var.project_name}/${var.environment}/network/vpc_id"
+}
+
+
+############################################
+# Private Subnets from SSM
+############################################
+
+data "aws_ssm_parameter" "private_subnet_ids" {
+  name = "/${var.project_name}/${var.environment}/network/private_subnet_ids"
+}
+
+
+############################################
+# App Security Group ID from SSM
+############################################
+
+data "aws_ssm_parameter" "app_security_group_id" {
+  name = "/${var.project_name}/${var.environment}/app/sg-id"
+}
+
+
+############################################
+# Locals
+############################################
+
+locals {
+  vpc_id = data.aws_ssm_parameter.vpc_id.value
+
+  private_subnet_ids = split(
+    ",",
+    data.aws_ssm_parameter.private_subnet_ids.value
+  )
+
+  app_security_group_id = data.aws_ssm_parameter.app_security_group_id.value
+}
