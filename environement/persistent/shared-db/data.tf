7bf783f403cfe1e518314681d715a7190317460c
kriiishamtic <kriiishmatic@gmail.com>
2026-08-19 10:38:26 +0530

    all

--- environement/persistent/shared-db/data.tf
+++ environement/persistent/shared-db/data.tf
@@ -20,9 +20,9 @@ data "aws_ssm_parameter" "private_subnet_ids" {
 # App Security Group ID from SSM
 ############################################
 
-data "aws_ssm_parameter" "app_security_group_id" {
-  name = "/${var.project_name}/${var.environment}/app/sg-id"
-}
+# data "aws_ssm_parameter" "app_security_group_id" {
+#   name = "/${var.project_name}/${var.environment}/app/sg-id"
+# }
 
 
 ############################################
